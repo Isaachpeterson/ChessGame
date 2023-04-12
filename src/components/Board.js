2105015299c6be82b02de217ad5bd619c338e2a9
Isaac Peterson <ihpetersonpersonal@gmail.com>
2023-04-12 11:21:53 -0500

    Initial commit for React frontend

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Board.css';
+
+class Board extends Component {
+  render() {
+    return (
+      <div className="Board">
+        {/* Chess board and pieces rendering will go here */}
+      </div>
+    );
+  }
+}
+
+export default Board;
